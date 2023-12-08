f5341c797505e73b72396eee41ba8c2aafaaaf9d
PhuriphatYoosuwan <phuriphat_yoosuwan@cmu.ac.th>
2023-12-08 16:36:40 +0700

    done

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -25,22 +25,22 @@ int main(){
     int id;
     cout << "Fahsai: Sawadee ka...Can you tell me your name?" <<endl;
     cout << "?????: ";
-    cin >> name;
+    getline(cin,name);
     cout << "Fahsai: Wow!!! "<< name <<" is a really cool name."<<endl;
     cout << "Fahsai: I think you are an Engineering student. What is your student ID?" <<endl;
     cout << name << ": ";
     cin >> id;
-    cout << "Fahsai: I think you may be GEAR " << (id/10000000)-12 <<". I have a free movie ticket for you.";
+    cin.ignore();
+    cout << "Fahsai: I think you may be GEAR " << (id/10000000)-12 <<". I have a free movie ticket for you." <<endl;
     cout << "Fahsai: Let's go to the cinema together!!!" <<endl;
     cout << "Fahsai: What movie do you want to watch?" <<endl;
     cout << name << ": ";
-    cin >> movie;
+    getline(cin,movie);
     cout << "Fahsai: So....which day are you free to go with me?" <<endl;
     cout << name << ": ";
-    cin >> day;
+    getline(cin,day);
     cout << "Fahsai: " << day << "....that is OK!!! I'm looking forward to watching " << movie << " with you." <<endl;
     cout << name <<": ";
     cin >> massage; 
-    cout << "Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/";
-    return 0;
+    cout << "Fahsai: 555+ see you " << day << ". Bye Bye \\(^ ^)/";
 }
